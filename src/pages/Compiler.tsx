536dec99e390ea92a912bb70180a8314edf15f21
Aman Chand <amanchandinc@gmail.com>
2024-03-21 20:13:40 +0545

    Updated warning

--- src/pages/Compiler.tsx
+++ src/pages/Compiler.tsx
@@ -255,7 +255,7 @@ function Compiler() {
       toast({
         title: 'Thank You For Your Patience 💚',
         description:
-          'Compilation maybe a bit slow on the first try but after that it runs smoothly'
+          'Compilation maybe a bit slow for the first time as we are on a free tier'
       });
 
       setWarn('Shown');
